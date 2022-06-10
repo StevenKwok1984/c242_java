eee05d1bc48b6b62ec9d12f1744bdf7907cc51f5
Steven Kwok <derekkwok2020@gmail.com>
2022-06-10 13:52:20 +0100

    gg

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    /*
+    public static void main(String[] args)
+    This is the entry point for the program.
+    Prints "Hello, World!" to the console.
+*/
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
