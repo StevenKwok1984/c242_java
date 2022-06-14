48457b713f886f572e5e2eb700212e50ea84e612
Steven Kwok <derekkwok2020@gmail.com>
2022-06-14 11:46:57 +0100

    Update MiniZork.java

--- ExPP4_IfElse/MiniZork.java
+++ ExPP4_IfElse/MiniZork.java
@@ -1,7 +1,27 @@
 package ExPP4_IfElse;
 
+import java.util.Scanner;
+
 public class MiniZork {
     public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("You are standing in an open field west of a white house,");
+        System.out.println("With a boarded front door.");
+        System.out.println("There is a small mailbox here.");
+        System.out.print("Go to the house, or open the mailbox? ");
+
+        String action = userInput.nextLine();
+
+        if (action.equals("open the mailbox")) {
+            System.out.println("You open the mailbox.");
+            System.out.println("It's ");
+        }
+
+
+
+
 
     }
 }
