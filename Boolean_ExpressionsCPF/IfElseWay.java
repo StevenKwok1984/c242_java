a63d9630f9af0ac01287c5fccd3fff9b2aadfcf4
Steven Kwok <derekkwok2020@gmail.com>
2022-06-14 09:04:35 +0100

    BooleanExpression
    Whole part completed

---
+++ Boolean_ExpressionsCPF/IfElseWay.java
@@ -0,0 +1,29 @@
+package Boolean_ExpressionsCPF;
+
+public class IfElseWay {
+    public static void main(String[] args){
+        int day = 4;
+            String dayName = "";
+
+            if (day == 1) {
+                dayName = "Monday";
+            } else if (day == 2) {
+                dayName = "Tuesday";
+            } else if (day == 3) {
+                dayName ="Wednesday";
+            } else if (day == 4) {
+                dayName = "Thursday";
+            } else if (day == 5) {
+                dayName = "Friday";
+            } else if (day == 6) {
+                dayName = "Saturday";
+            } else if (day == 7) {
+                dayName = "Sunday";
+            } else {
+                dayName = "an invalid day";
+            }
+
+            System.out.println(dayName);
+
+    }
+}
---
+++ Boolean_ExpressionsCPF/WeekSwitch.java
@@ -0,0 +1,27 @@
+package Boolean_ExpressionsCPF;
+
+public class WeekSwitch {
+    public static void main(String[] args){
+        int day = 4;
+        String dayType = "";
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                dayType = "Weekday";
+                break;
+            case 6:
+            case 7:
+                dayType = "Weekend";
+                break;
+            default:
+                dayType = "Invalid Day";
+        }
+
+        System.out.println(dayType);
+
+    }
+}
